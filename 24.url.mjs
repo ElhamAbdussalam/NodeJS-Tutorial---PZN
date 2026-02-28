f225947ae0383f70af4c6c4ac863d1dc1c9cec7b
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-02-28 13:58:50 +0700

    url

--- 24.url.mjs
+++ 24.url.mjs
@@ -2,6 +2,9 @@ import { URL } from "url";
 
 const pzn = new URL("https://elhamabdussalam.vercel.app/home?name=elham");
 
+pzn.hostname = "elhamabdussalam.vercel.app";
+pzn.searchParams.append("name", "elhamabdussalam");
+
 console.info(pzn.toString());
 console.info(pzn.href);
 console.info(pzn.protocol);
