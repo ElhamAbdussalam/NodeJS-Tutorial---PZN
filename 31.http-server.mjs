01fe6f67a1eff5d44e1fa5080f01027532e90de1
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-03-02 05:21:33 +0700

    http-server.mjs

--- 31.http-server.mjs
+++ 31.http-server.mjs
@@ -3,10 +3,21 @@ import http from "http";
 const server = http.createServer((request, response) => {
   console.info(request.method);
   console.info(request.url);
-  console.table(request.headers);
 
-  response.write("Hello world");
-  response.end();
+  if (request.method === "POST") {
+    request.addListener("data", (data) => {
+      response.setHeader("Content-Type", "application/json");
+      response.write(data);
+      response.end();
+    });
+  } else {
+    if (request.url === "/eko") {
+      response.write("Hello Eko");
+    } else {
+      response.write("Hello World");
+    }
+    response.end();
+  }
 });
 
 server.listen(3000, "localhost");
