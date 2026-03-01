c7549fdafe8dc5e4732f30261722343a0efb0766
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-03-02 05:17:11 +0700

    http-server.mjs

---
+++ 31.http-server.mjs
@@ -0,0 +1,12 @@
+import http from "http";
+
+const server = http.createServer((request, response) => {
+  console.info(request.method);
+  console.info(request.url);
+  console.table(request.headers);
+
+  response.write("Hello world");
+  response.end();
+});
+
+server.listen(3000, "localhost");
