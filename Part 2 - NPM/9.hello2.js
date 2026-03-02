314da83d28d65be37da9d9441cfaf9a87e821f5e
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-03-02 19:52:32 +0700

    hello

---
+++ Part 2 - NPM/9.hello2.js
@@ -0,0 +1,10 @@
+import { sayHello, sum } from "npm-library-elhamabdussalam";
+import { min, max } from "./8.number";
+
+console.info(sayHello("Elham"));
+
+const numbers = [10, 20, 50, 70];
+console.info(sum(numbers));
+
+console.info(min(10, 20));
+console.info(max(10, 20));
---
+++ Part 2 - NPM/node_modules/npm-library-elhamabdussalam/3.index.js
@@ -0,0 +1,3 @@
+import { writeToFile } from "./2.write.js";
+
+writeToFile("hello.log", "Hello world");
