b45d6a96cf9aec31b19133be35fd90ef2d387fc3
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-03-02 19:46:25 +0700

    update npm

---
+++ Part 2 - NPM/8.number.js
@@ -0,0 +1,17 @@
+import { first } from "lodash";
+
+export const min = (first, second) => {
+  if (first < second) {
+    return first;
+  } else {
+    return second;
+  }
+};
+
+export const max = (first, second) => {
+  if (first > second) {
+    return first;
+  } else {
+    return second;
+  }
+};
