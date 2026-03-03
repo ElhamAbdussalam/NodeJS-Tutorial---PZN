ed70f43f0c4be356411b3f0a6ab5ee33b4bdbc24
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-03-03 21:10:02 +0700

    async

---
+++ Part 3 - Unit Testing/src/2.async.js
@@ -0,0 +1,13 @@
+const sayHelloAsync = (name) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (name) {
+        resolve(`Hello ${name}`);
+      } else {
+        reject(new Error("Name is empty"));
+      }
+    }, 1000);
+  });
+};
+
+export default sayHelloAsync;
---
+++ Part 3 - Unit Testing/test/9.async.test.js
@@ -0,0 +1,6 @@
+// import { sayHelloAsync } from "../src/2.async.js";
+
+// test("test async function", async () => {
+//   const result = await sayHelloAsync("Eko");
+//   expect(result).toBe("Hello Eko");
+// });
