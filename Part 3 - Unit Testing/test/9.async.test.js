bb824be5cba55eab64e710bf041e8b7a9a4328a2
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-03-03 21:31:48 +0700

    async

--- Part 3 - Unit Testing/test/9.async.test.js
+++ Part 3 - Unit Testing/test/9.async.test.js
@@ -4,3 +4,8 @@
 //   const result = await sayHelloAsync("Eko");
 //   expect(result).toBe("Hello Eko");
 // });
+
+// test("test async matchers", async () => {
+//   await expect(sayHelloAsync("Eko")).resolves.toBe("Hello Eko");
+//   await expect(sayHelloAsync()).rejects.toThrow("Name is empty");
+// });
