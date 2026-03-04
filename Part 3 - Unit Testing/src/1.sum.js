ea9e5b22a82294ca96c51dff35a025ee7c4e085d
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-03-04 15:54:37 +0700

    mock function

--- Part 3 - Unit Testing/src/1.sum.js
+++ Part 3 - Unit Testing/src/1.sum.js
@@ -18,4 +18,12 @@ const calculate = (numbers, callback) => {
   callback(total);
 };
 
-module.exports = { sum, sumAll, calculate };
+const calculateAndReturn = (numbers, callback) => {
+  let total = 0;
+  for (let number of numbers) {
+    total += number;
+  }
+  return callback(total);
+};
+
+module.exports = { sum, sumAll, calculate, calculateAndReturn };
--- Part 3 - Unit Testing/test/20.mock-function.test.js
+++ Part 3 - Unit Testing/test/20.mock-function.test.js
@@ -1,4 +1,4 @@
-import { calculate } from "../src/1.sum";
+import { calculate, calculateAndReturn } from "../src/1.sum";
 
 test("test calculate", () => {
   const callback = jest.fn();
@@ -16,4 +16,16 @@ test("test sumAll", () => {
   expect(callback.mock.calls.length).toBe(2);
   expect(callback.mock.calls[0][0]).toBe(30);
   expect(callback.mock.calls[1][0]).toBe(50);
+});
+
+test("test mock return value", () => {
+  const callback = jest.fn();
+  callback.mockReturnValueOnce(40);
+  callback.mockReturnValueOnce(80);
+
+  expect(calculateAndReturn([10, 10], callback)).toBe(40);
+  expect(calculateAndReturn([10, 10, 10, 10], callback)).toBe(80);
+
+  expect(callback.mock.results[0].value).toBe(40);
+  expect(callback.mock.results[1].value).toBe(80);
 });
