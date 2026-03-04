bed4c0b272c87e050d8c878c879f62d0a008dbeb
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-03-04 14:48:31 +0700

    each function

--- Part 3 - Unit Testing/test/16.each.test.js
+++ Part 3 - Unit Testing/test/16.each.test.js
@@ -7,3 +7,13 @@ test("sumAll(10,10,10)", () => {
 test("sumAll(10,10,10, 10, 10)", () => {
   expect(sumAll([10, 10, 10, 10, 10])).toBe(50);
 });
+
+const table = [
+  [[10, 10, 10], 30],
+  [[10, 10, 10, 10, 10], 50],
+  [[10, 10, 10, 10, 10, 10, 10], 70],
+];
+
+test.each(table)("test sumAll(%s) should result %i", (numbers, expected) => {
+  expect(sumAll(numbers)).toBe(expected);
+});
