18fe04d333da6c9d28513772472107a812f295fc
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-03-04 15:50:04 +0700

    mock function

--- Part 3 - Unit Testing/test/20.mock-function.test.js
+++ Part 3 - Unit Testing/test/20.mock-function.test.js
@@ -6,3 +6,14 @@ test("test calculate", () => {
   calculate([10, 10, 10], callback);
   calculate([10, 10, 10, 10, 10], callback);
 });
+
+test("test sumAll", () => {
+  const callback = jest.fn();
+
+  calculate([10, 10, 10], callback);
+  calculate([10, 10, 10, 10, 10], callback);
+
+  expect(callback.mock.calls.length).toBe(2);
+  expect(callback.mock.calls[0][0]).toBe(30);
+  expect(callback.mock.calls[1][0]).toBe(50);
+});
