c050edc113c16419e78b827a1c35866b11ba0a7e
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-03-05 15:27:14 +0700

    level

---
+++ Part 3 - Unit Testing and Logging/test/29.level.test.js
@@ -0,0 +1,15 @@
+import winston from "winston";
+
+test("logging with level", () => {
+  const logger = winston.createLogger({
+    transports: [new winston.transports.Console({})],
+  });
+
+  logger.log({ level: "error", message: "Error message" });
+  logger.log({ level: "warn", message: "Warn message" });
+  logger.log({ level: "info", message: "Info message" });
+  logger.log({ level: "http", message: "HTTP message" });
+  logger.log({ level: "verbose", message: "Verbose message" });
+  logger.log({ level: "debug", message: "Debug message" });
+  logger.log({ level: "silly", message: "Silly message" });
+});
