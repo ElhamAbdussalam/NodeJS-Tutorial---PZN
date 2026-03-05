2177df5a5c62b79814cef359ee38803cabedcc28
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-03-05 20:17:36 +0700

    transport

--- Part 3 - Unit Testing and Logging/test/36.transport.test.js
+++ Part 3 - Unit Testing and Logging/test/36.transport.test.js
@@ -1,3 +1,4 @@
+import winston from "winston";
 import DailyRotateFile from "winston-daily-rotate-file";
 import TransportStream from "winston-transport";
 
@@ -14,4 +15,16 @@ test("create new transport", () => {
       next();
     }
   }
+  const logger = winston.createLogger({
+    level: "silly",
+    transports: [new MyTransport({})],
+  });
+
+  logger.error("Error Message");
+  logger.warn("Warn Message");
+  logger.info("Info Message");
+  logger.http("HTTP Message");
+  logger.verbose("Verbose Message");
+  logger.debug("Debug Message");
+  logger.silly("Silly Message");
 });
