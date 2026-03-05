65ec4c3af621bd2e60c2bcf4c468cf5c440c9b60
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-03-05 20:13:46 +0700

    transport

---
+++ Part 3 - Unit Testing and Logging/test/36.transport.test.js
@@ -0,0 +1,17 @@
+import DailyRotateFile from "winston-daily-rotate-file";
+import TransportStream from "winston-transport";
+
+test("create new transport", () => {
+  class MyTransport extends TransportStream {
+    constructor(option) {
+      super(option);
+    }
+
+    log(info, next) {
+      console.log(
+        `${new Date()} : ${info.level.toUpperCase()} : ${info.message}`,
+      );
+      next();
+    }
+  }
+});
